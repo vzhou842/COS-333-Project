411b649479b158bd3ddf798ca3ba0bb83870a6ba
Victor Zhou <vzhou842@gmail.com>
2017-04-03 21:01:50 -0400

    Server: added app.yaml, debug GET /ping endpoint, node/npm versions to package.json

--- server/server.js
+++ server/server.js
@@ -10,3 +10,8 @@ var app = express();
 var port = process.env.PORT || 8080;
 var server = app.listen(port);
 console.log('Server listening on port ' + port);
+
+// Setup a debug endpoint.
+app.get('/ping', function(req, res) {
+	res.status(200).send('pong');
+});
