93451f005b9601cbd522aa395807f30600f4e8e5
Victor Zhou <vzhou842@gmail.com>
2017-04-03 22:53:47 -0400

    Server: setup body-parser and PostAPI file with POST /api/posts implementation

--- server/server.js
+++ server/server.js
@@ -11,7 +11,13 @@ var port = process.env.PORT || 8080;
 var server = app.listen(port);
 console.log('Server listening on port ' + port);
 
+// Enable a JSON body parser.
+app.use(require('body-parser').json({ type: 'application/json' }));
+
 // Setup a debug endpoint.
 app.get('/ping', function(req, res) {
 	res.status(200).send('pong');
 });
+
+// Setup the Posts API.
+require('./PostAPI')(app);
