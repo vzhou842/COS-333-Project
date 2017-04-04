1d9b2b59dd6d0d61844d3df14e995cf9e0c1b3b3
Sarah Zhou <sarah.zhou101@gmail.com>
2017-04-03 21:28:36 -0400

    compose new post view added

---
+++ ios/333/333/ComposeViewController.swift
@@ -0,0 +1,89 @@
+//
+//  ComposeViewController.swift
+//  333
+//
+//  Created by Sarah Zhou on 4/3/17.
+//  Copyright © 2017 333. All rights reserved.
+//
+
+import UIKit
+
+class ComposeViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextViewDelegate {
+
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var postTextView: UITextView!
+    
+    @IBAction func cancelButton(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    let imagePicker = UIImagePickerController()
+    
+    @IBAction func cameraRoll(_ sender: Any) {
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func camera(_ sender: Any) {
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .camera
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        
+        postTextView.delegate = self
+        postTextView.text = "What's on your mind?"
+        postTextView.textColor = UIColor.lightGray
+        
+        countLabel.text = "200"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func textViewDidChange(_ textView: UITextView) {
+        countLabel.text = "\(200 - textView.text.characters.count)"
+        if 200 - textView.text.characters.count < 0 {
+            countLabel.textColor = UIColor.red
+        } else {
+            countLabel.textColor = UIColor.darkGray
+        }
+    }
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.textColor == UIColor.lightGray {
+            textView.text = ""
+            textView.textColor = UIColor.black
+        }
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text == "" {
+            textView.text = "What's on your mind?"
+            textView.textColor = UIColor.lightGray
+        }
+    }
+
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
