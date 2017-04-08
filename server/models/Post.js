230cf53e60e4751617ca0bea75f9862ea4aa0c30
Victor Zhou <vzhou842@gmail.com>
2017-04-08 17:22:43 -0400

    Server: added location indices for Post

--- server/models/Post.js
+++ server/models/Post.js
@@ -9,14 +9,15 @@ var schema = new mongoose.Schema({
 	user_id: { $type: String, required: true },
 	timestamp: { $type: Date },
 	loc: {
-		type: String,
+		type: { $type: String, default: 'Point' },
 		coordinates: [Number],
 	},
-	radius: { $type: Number, default: 5 }, //miles
+	radius: { $type: Number, default: 5000 }, // meters
 	num_upvotes: { $type: Number, default: 0 },
 	num_comments: { $type: Number, default: 0 },
 }, { typeKey: '$type' });
 
-// TODO: add indicies.
+schema.index({ loc: '2dsphere', timestamp: -1 });
+schema.index({ loc: '2dsphere', num_upvotes: -1 });
 
 module.exports = mongoose.model('Post', schema);
