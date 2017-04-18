8aec2f29fe511b8d20774ffb7329cde2d789ed15
Victor Zhou <vzhou842@gmail.com>
2017-04-18 00:40:06 -0400

    Client: updated Networking.createPost() to use the new multipart/form-data Post creation API

--- ios/333/333/Networking.swift
+++ ios/333/333/Networking.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2017 333. All rights reserved.
 //
 
+import UIKit
 import Alamofire
 
 class Networking {
@@ -14,23 +15,60 @@ class Networking {
     static let dateFormatter = DateFormatter()
     static let niceDateFormatter: DateFormatter? = DateFormatter()
 
+    static func stringToData(s:String) -> Data {
+        return s.data(using: String.Encoding.utf8)!
+    }
+    static func floatToData(f:Float) -> Data {
+        return stringToData(s: String(describing: f))
+    }
+
     //Create a new post with the specified parameters.
-    static func createPost(text:String?, image_url:String?, user_id:String, lat:Float, long:Float) {
+    static func createPost(text:String?, image:UIImage?, user_id:String, lat:Float, long:Float) {
+        var request = try! URLRequest(url: baseurl.asURL().appendingPathComponent("/api/posts"))
+        request.httpMethod = HTTPMethod.post.rawValue
         
-        let parameters: Parameters = [
-            "text": text ?? "",
-            "image_url": image_url ?? "",
-            "user_id": user_id,
-            "lat": lat,
-            "long": long
-        ]
-        
-        Alamofire.request("\(baseurl)/api/posts", method: .post, parameters: parameters, encoding: JSONEncoding.default).responseJSON(completionHandler: { response in
-            
-            if let json = response.result.value {
-                print("JSON: \(json)")
+        Alamofire.upload(
+            multipartFormData: { multipartFormData in
+                if image != nil {
+                    guard let imageData = UIImageJPEGRepresentation(image!, 0.5) else {
+                        print("Could not get JPEG representation of UIImage")
+                        return
+                    }
+                    multipartFormData.append(imageData,
+                                             withName: "img",
+                                             fileName: "img.jpg",
+                                             mimeType: "image/jpeg")
+                }
+
+                if (text != nil) {
+                    multipartFormData.append(stringToData(s: text!), withName: "text")
+                }
+                multipartFormData.append(stringToData(s: user_id), withName: "user_id")
+                multipartFormData.append(floatToData(f: lat), withName: "lat")
+                multipartFormData.append(floatToData(f: long), withName: "long")
+            },
+            with: request,
+            encodingCompletion: { encodingResult in
+                switch encodingResult {
+                case .success(let upload, _, _):
+                    upload.validate()
+                    upload.responseJSON { response in
+                        guard response.result.isSuccess else {
+                            print("Error while creating post: \(String(describing: response.result.error))")
+                            return
+                        }
+
+                        guard let responseJSON = response.result.value as? [String: Any] else {
+                            print("Invalid response received when creating post")
+                            return
+                        }
+                        print("Successfully created post \(String(describing: responseJSON["post_id"]))")
+                    }
+                case .failure(let encodingError):
+                    print(encodingError)
+                }
             }
-        })
+        )
     }
     
     //Get all new posts and run the completion handler if successful.
