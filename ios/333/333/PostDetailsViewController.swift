22055189e7306e10526d32a2f2a063d389130931
Victor Zhou <vzhou842@gmail.com>
2017-05-08 18:09:34 -0400

    iOS: Refactored PostDetailsVC to use a tableview

--- ios/333/333/PostDetailsViewController.swift
+++ ios/333/333/PostDetailsViewController.swift
@@ -11,23 +11,13 @@ import UIKit
 class PostDetailsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate {
     
     //Outlets
-    @IBOutlet weak var commentsTableView: UITableView!
-    @IBOutlet weak var replyView: UIView!
+    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var replyTextField: UITextField!
-    @IBOutlet weak var upvotesCountLabel: UILabel!
-    @IBOutlet weak var timeStampLabel: UILabel!
-    @IBOutlet weak var replyCountLabel: UILabel!
-    @IBOutlet weak var captionLabel: UILabel!
-    @IBOutlet weak var noCommentsLabel: UIView!
-    
-    @IBOutlet weak var upButton: UIButton!
-    @IBOutlet weak var downButton: UIButton!
-    @IBOutlet weak var cityLabel: UILabel!
     
     @IBAction func refreshPost(_ sender: Any) {
         Networking.getComments(post_id: post.id) { (comments) in
             self.comments = comments
-            self.commentsTableView.reloadData()
+            self.tableView.reloadData()
             Toaster.makeToastBottom(self.view, "Refreshed!")
         }
     }
@@ -38,74 +28,23 @@ class PostDetailsViewController: UIViewController, UITableViewDataSource, UITabl
     var keyboardHeight = 300 as CGFloat
     let defaultReply = "Reply ..."
     
-    var didUpvote: Bool = false
-    var didDownvote: Bool = false
-    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(true)
-        
-        let defaults = UserDefaults.standard
-        didUpvote = defaults.bool(forKey: "up"+post.id)
-        didDownvote = defaults.bool(forKey: "down"+post.id)
-        
-        if (didUpvote) {
-            self.upButton.setImage(UIImage(named: "upvoteFilled"), for: .normal)
-        }
-        else if (didDownvote) {
-            self.downButton.setImage(UIImage(named: "downvoteFilled"), for: .normal)
-        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let defaults = UserDefaults.standard
-        didUpvote = defaults.bool(forKey: "up"+post.id)
-        didDownvote = defaults.bool(forKey: "down"+post.id)
-        
-        if (didUpvote) {
-            self.upButton.setImage(UIImage(named: "upvoteFilled"), for: .normal)
-        }
-        else {
-            self.upButton.setImage(UIImage(named: "upvote"), for: .normal)
-        }
-        if (didDownvote) {
-            self.downButton.setImage(UIImage(named: "downvoteFilled"), for: .normal)
-        }
-        else {
-            self.downButton.setImage(UIImage(named: "downvote"), for: .normal)
-        }
-        
-        commentsTableView.delegate = self
-        commentsTableView.dataSource = self
-        replyTextField.delegate = self
-        replyTextField.textColor = UIColor.lightGray
-        
-        //Set post properties
-        captionLabel.text = post.text
-        replyCountLabel.text = "\(post.numComments) comments"
-        if (post.numComments == 1)
-        {replyCountLabel.text = "\(post.numComments) comment"}
-        
-        cityLabel.text = post.city
-        
-        upvotesCountLabel.text = "\(post.numUpvotes)"
-        let timeInterval = post.date.timeIntervalSinceNow
-        timeStampLabel.text = "\(Utils.formatDate(-timeInterval)) ago"
-        
-        view.bringSubview(toFront: replyView)
-        replyView.isUserInteractionEnabled = true
-        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 300
+
         // Load comments
         Networking.getComments(post_id: post.id) { (comments) in
             self.comments = comments
-            self.commentsTableView.reloadData()
-            
-            if (comments.count != 0) { self.noCommentsLabel.isHidden = true }
-            else { self.commentsTableView.isHidden = true }
+            self.tableView.reloadData()
         }
-
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
@@ -114,41 +53,58 @@ class PostDetailsViewController: UIViewController, UITableViewDataSource, UITabl
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return comments?.count ?? 0
+        if let comments = comments {
+            print("\(comments.count + 1) rows")
+            return comments.count + 1
+        }
+        print("1 row")
+        return 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "replyTableViewCell") as! ReplyTableViewCell
-        
-        let comment = comments![(comments?.count)!-indexPath.row-1]
-        
-        let defaults = UserDefaults.standard
-        
-        //Set cell properties
-        cell.captionLabel.text = comment.text
-        cell.votesCountLabel.text = "\(comment.numUpvotes)"
-        cell.comment = comment
-        
-        let timeIntervalComment = comment.date.timeIntervalSinceNow
-        cell.timestampLabel.text = "\(Utils.formatDate(-timeIntervalComment)) ago"
-        
-        cell.didUpvote = defaults.bool(forKey: "up"+comment.comment_id)
-        cell.didDownvote = defaults.bool(forKey: "down"+comment.comment_id)
-        
-        if (cell.didUpvote)! {
-            cell.upButton.setImage(UIImage(named: "upvoteFilled"), for: .normal)
+        print("cell for row \(indexPath.row)")
+        if (indexPath.row == 0) {
+            // Post cell.
+            let cell = tableView.dequeueReusableCell(withIdentifier: "postTableViewCell") as! PostTableViewCell
+
+            // Configure this cell for its post.
+            cell.configureWithPost(self.post)
+
+            return cell
+        } else {
+            // Comment cell.
+            let cell = tableView.dequeueReusableCell(withIdentifier: "replyTableViewCell") as! ReplyTableViewCell
+
+            let comment = comments![(comments?.count)!-indexPath.row]
+
+            let defaults = UserDefaults.standard
+
+            //Set cell properties
+            cell.captionLabel.text = comment.text
+            cell.votesCountLabel.text = "\(comment.numUpvotes)"
+            cell.comment = comment
+
+            let timeIntervalComment = comment.date.timeIntervalSinceNow
+            cell.timestampLabel.text = "\(Utils.formatDate(-timeIntervalComment)) ago"
+
+            cell.didUpvote = defaults.bool(forKey: "up"+comment.comment_id)
+            cell.didDownvote = defaults.bool(forKey: "down"+comment.comment_id)
+
+            if (cell.didUpvote)! {
+                cell.upButton.setImage(UIImage(named: "upvoteFilled"), for: .normal)
+            }
+            else {
+                cell.upButton.setImage(UIImage(named: "upvote"), for: .normal)
+            }
+            if (cell.didDownvote)! {
+                cell.downButton.setImage(UIImage(named: "downvoteFilled"), for: .normal)
+            }
+            else {
+                cell.downButton.setImage(UIImage(named: "downvote"), for: .normal)
+            }
+
+            return cell
         }
-        else {
-            cell.upButton.setImage(UIImage(named: "upvote"), for: .normal)
-        }
-        if (cell.didDownvote)! {
-            cell.downButton.setImage(UIImage(named: "downvoteFilled"), for: .normal)
-        }
-        else {
-            cell.downButton.setImage(UIImage(named: "downvote"), for: .normal)
-        }
-        
-        return cell
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
@@ -181,93 +137,12 @@ class PostDetailsViewController: UIViewController, UITableViewDataSource, UITabl
         
         Networking.getComments(post_id: post.id) { (comments) in
             self.comments = comments
-            self.commentsTableView.reloadData()
+            self.tableView.reloadData()
         }
-        
-        noCommentsLabel.isHidden = true
-        commentsTableView.isHidden = false
     }
 
     @IBAction func back(_ sender: Any) {
         navigationController?.popViewController(animated: true)
-    }
-    
-    @IBAction func onTouchUpvote(_ sender: Any) {
-        let user_id = UIDevice.current.identifierForVendor!.uuidString
-        let object_id = post!.id
-        let up = true
-        
-        Networking.createVote(lat: Location.sharedInstance.lat, long: Location.sharedInstance.long, user_id: user_id, object_id: object_id, up: up, completion: {(success) in
-            if (!success) {
-                return
-            }
-            if (self.didUpvote){
-                self.upvotesCountLabel.text = "\(Int(self.upvotesCountLabel.text!)!-1)"
-                self.didUpvote = false
-                self.upButton.setImage(UIImage(named: "upvote"), for: .normal)
-                self.post?.numUpvotes -= 1
-            } else if (self.didDownvote) {
-                self.upvotesCountLabel.text = "\(Int(self.upvotesCountLabel.text!)!+2)"
-                self.didUpvote = true
-                self.didDownvote = false
-                self.downButton.setImage(UIImage(named: "downvote"), for: .normal)
-                self.upButton.setImage(UIImage(named: "upvoteFilled"), for: .normal)
-                self.post?.numUpvotes += 2
-            } else {
-                self.upvotesCountLabel.text = "\(Int(self.upvotesCountLabel.text!)!+1)"
-                self.didUpvote = true
-                self.upButton.setImage(UIImage(named: "upvoteFilled"), for: .normal)
-                self.post?.numUpvotes += 1
-            }
-            
-            print(self.didDownvote)
-            print(self.didUpvote)
-            let defaults = UserDefaults.standard
-            defaults.set(self.didUpvote, forKey: "up"+self.post.id)
-            defaults.set(self.didDownvote, forKey: "down"+self.post.id)
-            defaults.synchronize()
-            
-            print(self.post.numUpvotes)
-        })
-    }
-    
-    @IBAction func onTouchDownvote(_ sender: Any) {
-        let user_id = UIDevice.current.identifierForVendor!.uuidString
-        let object_id = post!.id
-        let up = false
-        
-        Networking.createVote(lat: Location.sharedInstance.lat, long: Location.sharedInstance.long, user_id: user_id, object_id: object_id, up: up, completion: {(success) in
-            if (!success) {
-                return
-            }
-            if (self.didUpvote){
-                self.upvotesCountLabel.text = "\(Int(self.upvotesCountLabel.text!)!-2)"
-                self.didUpvote = false
-                self.didDownvote = true
-                self.upButton.setImage(UIImage(named: "upvote"), for: .normal)
-                self.downButton.setImage(UIImage(named: "downvoteFilled"), for: .normal)
-                self.post?.numUpvotes -= 2
-            } else if (self.didDownvote) {
-                self.upvotesCountLabel.text = "\(Int(self.upvotesCountLabel.text!)!+1)"
-                self.didDownvote = false
-                self.downButton.setImage(UIImage(named: "downvote"), for: .normal)
-                self.post?.numUpvotes += 1
-            } else {
-                self.upvotesCountLabel.text = "\(Int(self.upvotesCountLabel.text!)!-1)"
-                self.didDownvote = true
-                self.downButton.setImage(UIImage(named: "downvoteFilled"), for: .normal)
-                self.post?.numUpvotes -= 1
-            }
-            
-            print(self.didDownvote)
-            print(self.didUpvote)
-            let defaults = UserDefaults.standard
-            defaults.set(self.didUpvote, forKey: "up"+self.post.id)
-            defaults.set(self.didDownvote, forKey: "down"+self.post.id)
-            defaults.synchronize()
-            
-            print(self.post.numUpvotes)
-        })
     }
 
     /*
