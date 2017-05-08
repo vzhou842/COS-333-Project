b2b01f79133851bb9d1721657624d97e2aa3c8b4
Victor Zhou <vzhou842@gmail.com>
2017-05-08 11:54:18 -0400

    iOS: added Reachability + Toasts pods, implemented the "No Internet Connection" toast

---
+++ ios/333/333/Toaster.swift
@@ -0,0 +1,22 @@
+//
+//  Toaster.swift
+//  333
+//
+//  Created by Victor Zhou on 5/8/17.
+//  Copyright © 2017 333. All rights reserved.
+//
+
+import Foundation
+import Toast_Swift
+
+class Toaster {
+    static let style: ToastStyle = {
+        var style = ToastStyle()
+        style.messageFont = UIFont.systemFont(ofSize: 14, weight: UIFontWeightThin)
+        return style
+    }()
+
+    static func makeToast(_ view: UIView, _ message: String) {
+        view.makeToast(message, duration: 3.0, position: ToastPosition.bottom, style: style)
+    }
+}
